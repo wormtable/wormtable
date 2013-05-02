0395c46abc86158aef573ecd7d29a4c378081b6d
Jerome Kelleher <jerome.kelleher@ed.ac.uk>
2013-05-02 21:12:47 +0100

    Finished allele frequency example.

--- dev.py
+++ dev.py
@@ -3,9 +3,11 @@ This is an example file get started with building a wormtable for
 a VCF file, dumping out the values, and reading indexes.
 """
 from __future__ import print_function
+from __future__ import division 
 
 import sys
 
+import numpy as np
 import wormtable as wt
 
 def read_position_index(homedir, chromosome, position):
@@ -76,25 +78,55 @@ def allele_frequency_example(homedir):
     table = wt.Table(homedir) 
     index = wt.Index(table, ["CHROM", "POS"]) 
     index.open()
-    chromosome = b"1"
+    chromosome = b"2"
     min_val = (chromosome, 0)
     max_val = (chromosome, 2**32) # some big value
+    
+    # Two casese are covered in the code here; when we have INFO_AF and 
+    # when we don't. Uncomment the code to try each one out.
+    # If we have INFO_AF, we may as well use it
+    # read_cols = ['INFO_AF'] 
+    
+    # Otherwise, we list the genotypes. We are only making an example, so 
+    # it's totally fine to be specific to a given VCF. It would be good if
+    # this VCF was publically available, so we can say 'wget http://...vcf'.
     genotypes = ["Fam_GT", "H12_GT", "H14_GT", "H15_GT", "H24_GT", "H26_GT",
             "H27_GT", "H28_GT", "H30_GT", "H34_GT", "H36_GT"]
     num_genotypes = len(genotypes)
-    read_cols = ["ALT", "INFO_AF"] + genotypes 
+    read_cols = ["ALT"] + genotypes 
+    j = 0
+    # Allocate a zeroed numpy array to store the allele frequencies for the 
+    # whole chromosome. This is 100 million values, but still requires less
+    # than a gig of memory. That's memory well spent!
+    a = np.zeros(int(1e8)) 
     for row in index.get_rows(read_cols, min_val, max_val): 
-        print(row)
-        af = 0.0
-        for g in row[2:]:
-            if g != "0/0":
-                af += 1
-                if g == "1/1":
+        # this is the code for when we have INFO_AF
+        #if row[0] != None:
+        #    af = row[0][0]
+    
+        # And this is code for when we don't
+        if row[0] != '':
+            af = 0.0
+            # This is wrong, but should be easy to correct...
+            for g in row[1:]:
+                if g != b"0/0":
                     af += 1
-        af /= 2 * num_genotypes
-        print(af)
+                    if g == b"1/1":
+                        af += 1
+            af /= 2 * num_genotypes
+        # Now insert the allele frequency into our big array. 
+        a[j] = af
+        j += 1 
     index.close()
     table.close()
+    # trim off the end of the array we didn't use
+    a = a[:j]
+    # now we have the full array of allele frequecy values, use numpy 
+    # to do the stats on it. This would be by far the most efficient 
+    # way to do the sliding window stuff.
+    print("total rows = ", j)
+    print("mean = ", np.mean(a))
+    print("var = ", np.var(a))
 
 
 
