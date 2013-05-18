07aca3da6527d5bfbf20b700a110212418d17eaf
Jerome Kelleher <jerome.kelleher@ed.ac.uk>
2013-05-18 15:17:48 +0100

    Fixed style issues on example file.

--- examples/hist.py
+++ examples/hist.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 """
 Create a histogram for a numeric index.
 Usage: hist.py homedir index
@@ -11,5 +9,6 @@ import wormtable as wt
 table = wt.Table(sys.argv[1]) 
 index = wt.Index(table, [sys.argv[2]])
 index.open()
-[print(x,"\t",index.get_num_rows(x)) for x in index.get_distinct_values()]
+for x in index.get_distinct_values():
+    print(x,"\t",index.get_num_rows(x)
    
