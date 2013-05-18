e47a934c3952fa4609bc3fd352c6ab4e562eafe0
Jerome Kelleher <jerome.kelleher@ed.ac.uk>
2013-05-18 15:25:59 +0100

    Fixed bug in hist example.

--- examples/hist.py
+++ examples/hist.py
@@ -2,6 +2,8 @@
 Create a histogram for a numeric index.
 Usage: hist.py homedir index
 """
+from __future__ import print_function
+from __future__ import division
 
 import sys
 import wormtable as wt
@@ -10,5 +12,5 @@ table = wt.Table(sys.argv[1])
 index = wt.Index(table, [sys.argv[2]])
 index.open()
 for x in index.get_distinct_values():
-    print(x,"\t",index.get_num_rows(x)
+    print(x,"\t",index.get_num_rows(x))
    
