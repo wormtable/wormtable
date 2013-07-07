4143918e11757ea5d21267065085b73691c07cec
Jerome Kelleher <jerome.kelleher@ed.ac.uk>
2013-07-07 10:49:20 +0100

    Tidied up ts-tv example.

--- examples/count-ts-tv-methods.py
+++ examples/count-ts-tv-methods.py
@@ -14,43 +14,37 @@ import vcf
 from itertools import permutations
 import wormtable as wt
 
-bases = {'A':'purine', 'G':'purine', 'C':'pyrimidine', 'T':'pyrimidine'}
+bases = {b'A':b'purine', b'G':b'purine', b'C':b'pyrimidine', b'T':b'pyrimidine'}
 
 def count_Ts_Tv_pyvcf(vcf_file):
-	""" 
-	Count number of transitions and transversions using pyVCF
-	"""
-	vcf_reader = vcf.Reader(filename=vcf_file)
-	Ts, Tv = 0, 0
-	for record in vcf_reader:
-		if str(record.REF) in bases.keys() and \
-		record.ALT[0] in bases.keys() \
-		and len(record.ALT) == 1:
-			if bases[record.REF] == bases[str(record.ALT[0])]:
-				Ts +=1
-			else:
-				Tv +=1
-	return Ts, Tv
+    """ 
+    Count number of transitions and transversions using pyVCF
+    """
+    vcf_reader = vcf.Reader(filename=vcf_file)
+    Ts, Tv = 0, 0
+    for r in vcf_reader:
+        if r.REF in bases and r.ALT[0] in bases and len(r.ALT) == 1:
+            if bases[r.REF] == bases[r.ALT[0]]:
+                Ts +=1
+            else:
+                Tv +=1
+    return Ts, Tv
 
 
 def count_Ts_Tv_wtcursor(homedir):
-	""" 
-	Count number of transitions and transversions using wormtable and an 
-	index on CHROM+POS, counting Ts and Tv row by row
-	"""
-	t = wt.open_table(homedir)
-	i = t.open_index("CHROM+POS")
-	c = t.cursor(["REF","ALT"], i)
-	Ts, Tv = 0, 0
-	for row in c:
-		if str(row[0]) in bases.keys() and str(row[1]) in bases.keys():
-			if bases[str(row[0])] == bases[str(row[1])]:
-				Ts +=1
-			else:
-				Tv +=1
-	i.close()
-	t.close()
-	return Ts, Tv
+    """ 
+    Count number of transitions and transversions using wormtable and an 
+    index on CHROM+POS, counting Ts and Tv row by row
+    """
+    with wt.open_table(homedir) as t:
+        Ts, Tv = 0, 0
+        for ref, alt in t.cursor(["REF", "ALT"]):
+            if ref in bases and alt in bases:
+                if bases[ref] == bases[alt]:
+                    Ts +=1
+                else:
+                    Tv +=1
+    return Ts, Tv
 
 
 def count_Ts_Tv_wtindex(homedir):
@@ -58,34 +52,35 @@ def count_Ts_Tv_wtindex(homedir):
     Count number of of transitions and transversions using wormtable and 
     an index on REF+ALT
     """
-    subs = [p for p in permutations([b'A',b'C',b'G',b'T'], 2)]
-    t = wt.open_table(homedir)
-    i = t.open_index("REF+ALT")
-    Ts, Tv = 0, 0
-    c = i.counter()
-    for s in subs:
-        if bases[s[0]] == bases[s[1]]: 
-            Ts += c[s] 
-        else: 
-            Tv += c[s] 
-    i.close()
-    t.close()
+    subs = [p for p in permutations(bases.keys(), 2)]
+    print(subs)
+    with wt.open_table(homedir) as t, t.open_index("REF+ALT") as i:
+        Ts, Tv = 0, 0
+        c = i.counter()
+        for s in subs:
+            if bases[s[0]] == bases[s[1]]: 
+                Ts += c[s] 
+            else: 
+                Tv += c[s] 
     return Ts, Tv
 
 
 def main():
+    if len(sys.argv) != 3:
+        s = "usage: {0} [pyvcf|wtcursor|wtindex] homedir".format(sys.argv[0])
+        sys.exit(s)
     method, homedir = sys.argv[1:]
     if method == 'pyvcf':
-    	Ts, Tv = count_Ts_Tv_pyvcf(homedir)
+        Ts, Tv = count_Ts_Tv_pyvcf(homedir)
     elif method == 'wtcursor':
-    	Ts, Tv = count_Ts_Tv_wtcursor(homedir)
+        Ts, Tv = count_Ts_Tv_wtcursor(homedir)
     elif method == 'wtindex':
-    	Ts, Tv = count_Ts_Tv_wtindex(homedir)
+        Ts, Tv = count_Ts_Tv_wtindex(homedir)
     else: 
         sys.exit("Method %s not recognised" %(method))
     
-    print("ts: %d tv: %d" % (Ts, Tv))
+    print("ts: {0} tv: {1}".format(Ts, Tv))
 
-	
+    
 if __name__ == "__main__":
-	main()
+    main()
