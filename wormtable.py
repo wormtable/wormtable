1631bfa340a3d7de4820d4da6189642f3b7767f7
Jerome Kelleher <jerome.kelleher@ed.ac.uk>
2013-08-01 13:04:56 +0100

    Added initial version of the gtf2wt converter.

--- wormtable.py
+++ wormtable.py
@@ -1257,4 +1257,111 @@ class ProgressMonitor(object):
         """
         print()
 
+BROKEN_GZIP_MESSAGE = """
+An error occurred reading the input gzip file. This is probably due to a
+bug in recent versions of Python, resulting in an error when trying 
+to read BGZF files. See http://bugs.python.org/issue17666 
+To convert this file to wormtable, you must decompress it first using gunzip.
+"""
 
+class FileReader(object):
+    """
+    A class for reading data files from a variety of sources and 
+    with progress updating.
+    """
+    def __init__(self, in_file):
+        if in_file == '-':
+            self.__input_file = sys.stdin
+            if sys.version_info[:2] >= (3, 1):
+                try:
+                    self.__input_file = sys.stdin.buffer
+                except AttributeError:
+                    # When we're testing, we replace stdin with an ordinary
+                    # file. This does not support buffer, but is also not 
+                    # needed so we can skip this step
+                    pass
+            self.__progress_monitor = None
+            self.__input_file_size = None
+            self.__progress_file = None
+        else:
+            if in_file.endswith(".gz"):
+                # Detect broken GZIP handling in 2.7/3.2 and others and abort
+                # TODO this has been fixed upstream and can be removed at 
+                # some point.
+                f = gzip.open(in_file, "rb")
+                try:
+                    s = f.readline()
+                except Exception as e:
+                    print(BROKEN_GZIP_MESSAGE)
+                    sys.exit(1)
+                f.close() 
+                # Carry on as before
+                self.__input_file = gzip.open(in_file, "rb")
+                self.__progress_file = self.__input_file.fileobj
+            else:
+                self.__input_file = open(in_file, "rb")
+                self.__progress_file = self.__input_file 
+            statinfo = os.stat(in_file)
+            self.__input_file_size = statinfo.st_size 
+        self.__progress_update_rows = 2**32 
+        self.__progress_monitor = None
+
+    def get_progress_update_rows(self):
+        """
+        Returns the number of rows after which we should update the progress 
+        monitor.
+        """
+        return self.__progress_update_rows
+
+    def set_progress_update_rows(self, update_rows):
+        """
+        Sets the number of rows after which we should update progress 
+        to the specified value.
+        """
+        self.__progress_update_rows = update_rows
+
+    def get_input_file(self):
+        """
+        Returns the File object that is the source of the information 
+        in this reader.
+        """
+        return self.__input_file
+
+    def set_progress(self, progress):
+        """
+        If progress is True turn on progress monitoring for this GTF reader.
+        """
+        if progress:
+            self.__progress_monitor = ProgressMonitor(self.__input_file_size, 
+                    "bytes")
+            self.__progress_monitor.update(0)
+            self.__progress_update_rows = 100
+            if self.__input_file_size > 2**30:
+                self.__progress_update_rows = 1000
+    
+    def update_progress(self):
+        """
+        Reads the position we are at in the underlying file and uses this to 
+        update the progress bar, if used.
+        """
+        if self.__progress_monitor is not None:
+            t = self.__progress_file.tell() 
+            self.__progress_monitor.update(t)
+
+    def finish_progress(self):
+        """
+        Finishes up the progress monitor, if in use.
+        """ 
+        if self.__progress_monitor is not None:
+            self.update_progress()
+            self.__progress_monitor.finish()
+
+    def close(self):
+        """
+        Closes any open files on this Reader.
+        """
+        self.__input_file.close()
+        if self.__progress_file is not None: 
+            self.__progress_file.close()
+
+ 
